078080676ef4731e543b7ed08db68d2c10def819
romerogianella <romerogianellaa@gmail.com>
2022-04-11 17:18:29 -0300

    trabajo viernes

---
+++ trabajo2/js/scripts/funciones.js
@@ -0,0 +1,14 @@
+
+function hola(){
+alert('hola');}
+
+  function chau(){
+    alert('chau');}
+     function pasar(){
+       alert();}
+         function cambiar(){
+           document.getElementById("a1").style.color="red";
+          document.getElementById("a1").style.background="grey";}
+          function volver(){
+            document.getElementById("a1").style.color="black";
+            document.getElementById("a1").style.background="blue";}
