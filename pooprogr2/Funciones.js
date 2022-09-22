b0db9c6bcb4a02aefd62991c9306d959f06edfdb
gianellaromero <romerogianellaa@gmail.com>
2022-09-22 00:26:18 -0300

    poo2

--- pooprogr2/Funciones.js
+++ pooprogr2/Funciones.js
@@ -1,15 +1,13 @@
 
-
-
     function valor(valor) {
         if (valor == "Comercial") {
-            document.getElementById("hola").innerHTML = 'Comisión: <br> <input type="text" name="comision" style="font-size: 10px;"></input>';
+            document.getElementById("tipo").innerHTML = 'Comisión: <br> <input type="text" name="comision" style="font-size: 10px;"></input>';
         } else{}
         if (valor == "Repartidor") {
-            document.getElementById("hola").innerHTML= 'Zona: <br> <input type="text" name="zona"style="font-size: 10px;" ></input>';
+            document.getElementById("tipo").innerHTML= 'Zona: <br> <input type="text" name="zona"style="font-size: 10px;" ></input>';
         } else{}
         if(valor=="Seleccionar"){
-         document.getElementById("hola").innerHTML = ""
+         document.getElementById("tipo").innerHTML = ""
     
         
     }}
