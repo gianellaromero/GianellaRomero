06e81556a8018ba46cec941652acf5434785e02a
gianellaromero <romerogianellaa@gmail.com>
2023-04-27 21:20:37 -0300

    Prueba onmouseover y alert

--- trabajo2/js/scripts/funciones.js
+++ trabajo2/js/scripts/funciones.js
@@ -1,14 +1,18 @@
+function hola() {
+  alert("Hola");
+}
 
-function hola(){
-alert('hola');}
-
-  function chau(){
-    alert('chau');}
-     function pasar(){
-       alert();}
-         function cambiar(){
-           document.getElementById("a1").style.color="red";
-          document.getElementById("a1").style.background="grey";}
-          function volver(){
-            document.getElementById("a1").style.color="black";
-            document.getElementById("a1").style.background="blue";}
+function chau() {
+  alert("Chau");
+}
+function pasar() {
+  alert();
+}
+function cambiar() {
+  document.getElementById("a1").style.color = "red";
+  document.getElementById("a1").style.background = "grey";
+}
+function volver() {
+  document.getElementById("a1").style.color = "black";
+  document.getElementById("a1").style.background = "blue";
+}
