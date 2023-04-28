7e28dd7aa94109270cc724d8f505433ac06e9e00
gianellaromero <romerogianellaa@gmail.com>
2023-04-27 21:30:14 -0300

    Prueba suma

--- trabajo3/funciones.js
+++ trabajo3/funciones.js
@@ -1,18 +1,17 @@
-function sumar(num1,num2) {
-let resultado = num1+num2;
-console.log(resultado);
+function sumar(num1, num2) {
+  let resultado = num1 + num2;
+  //console.log(resultado);
 }
 
-function sumar1(){
+function sumar1() {
+  let numb1 = document.getElementById("numero_a").value;
+  let numb2 = document.getElementById("numero_b").value;
+  console.log(numb1);
+  console.log(numb2);
+  let resul = Number(numb1) + Number(numb2);
+  console.log(resul);
 
-    let numb1 = document.getElementById("numero_a").value; 
-    let numb2 = document.getElementById("numero_b").value;
-    console.log(numb1);
-    console.log(numb2);
-    let resul = Number(numb1) + Number(numb2);
-   console.log (resul);
-
-   /*if (numb1>numb2){
+  /*if (numb1>numb2){
      (numb1);
      alert ("a es mayor");
     }
@@ -20,6 +19,5 @@ function sumar1(){
     (numb2);
      alert ("b es mayor");
    }*/
-    document.getElementById("r").innerHTML="Resultado: " + resul;
-   
+  document.getElementById("resultado").innerHTML = "Resultado: " + resul;
 }
