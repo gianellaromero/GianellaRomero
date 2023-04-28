81fdf958f7f9cb343db166b3622bb8f7bcf64414
gianellaromero <romerogianellaa@gmail.com>
2023-04-27 21:36:05 -0300

    Prueba en consola

--- trabajo4/funciones.js
+++ trabajo4/funciones.js
@@ -1,21 +1,24 @@
-function arreglo(){
+function arreglo() {
+  let miarreglo = [99, 4, 34, 93, 1];
 
-let miarreglo = [99,4,34,93,1];
-
-/*ordenar 
+  /*ordenar 
 mostrar por consola los numeros mayores a 50
 */
-Number= miarreglo
-console.log("mi arreglo: " + miarreglo);
-console.log("arreglo ordenado: "+ miarreglo.sort(function(a,b){return a-b}));
+  Number = miarreglo;
+  console.log("mi arreglo: " + miarreglo);
+  console.log(
+    "arreglo ordenado: " +
+      miarreglo.sort(function (a, b) {
+        return a - b;
+      })
+  );
 
-for (let i=0 ; i<miarreglo.length; i++){
-
-if (miarreglo[i]>50){
-console.log("mayor a 50: " + miarreglo[i])
-}
-if (miarreglo[i]%2==0){
-    console.log("numero par: " + miarreglo[i])
+  for (let i = 0; i < miarreglo.length; i++) {
+    if (miarreglo[i] > 50) {
+      console.log("mayor a 50: " + miarreglo[i]);
+    }
+    if (miarreglo[i] % 2 == 0) {
+      console.log("numero par: " + miarreglo[i]);
     }
   }
 }
